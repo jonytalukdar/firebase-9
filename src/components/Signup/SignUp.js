b6f42356d5795819d827b5225fcc025c7dbc058c
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-15 15:38:25 +0600

    added

---
+++ src/components/Signup/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Card, Button, Form } from 'react-bootstrap';
+
+const SignUp = () => {
+  return (
+    <>
+      <Card></Card>
+      <div className="w-100 text-centere">Already have an account? Log In</div>
+    </>
+  );
+};
+
+export default SignUp;
