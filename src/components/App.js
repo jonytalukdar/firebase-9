800b8c1a0ca1133ab44d79999a114270dca097ac
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-15 15:57:01 +0600

    added signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,16 @@
+import { Container } from 'react-bootstrap';
+import SignUp from './Signup/SignUp';
+
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: '100vh' }}
+    >
+      <div className="w-100" style={{ maxWidth: '400px' }}>
+        <SignUp />;
+      </div>
+    </Container>
   );
 }
 
