0622036e60d6056e45c3beeebd4b405ea2e0741b
jonytalukdar <jonytalukdar30@gmail.com>
2021-11-20 11:23:49 +0600

    added new file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AuthProvider from './context/auth-conext';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import {
+  ChakraProvider,
+  ColorModeScript,
+  extendTheme,
+  theme,
+} from '@chakra-ui/react'
+
+const customTheme = extendTheme({
+  config: {
+    initialColorMode: 'light',
+    useSystemColorMode: false,
+  },
+  colors: {
+    primary: theme.colors.pink,
+  },
+})
 
 ReactDOM.render(
-  <Router>
-    <AuthProvider>
+  <React.StrictMode>
+    <ChakraProvider theme={customTheme}>
+      <ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
       <App />
-    </AuthProvider>
-  </Router>,
+    </ChakraProvider>
+  </React.StrictMode>,
   document.getElementById('root')
-);
+)
